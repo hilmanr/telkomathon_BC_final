358f7d33a6e61d787838f6886f0a79e23b9cfb09
Hilman Ramadhan <hilmanrmdhn@gmail.com>
2022-07-04 20:11:16 +0800

    Update main

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
-import System.IO (hFlush, stdout)
+import System.IO
 import Data.Maybe 
+import Helper
+import Model
+
 
 -- Main Program
 mainProgram :: Valins -> String -> IO()
@@ -9,8 +12,6 @@ mainProgram :: Valins -> String -> IO()
 mainProgram valinsCurrent mode = do
 	case mode of 
 		"new_valins" -> do
-			putStrLn "\n\n\n----------- VALINS PROCESSOR -----------"
-			putStrLn "----------------------------------------"
 			userInput <- getUserInput "Masukkan ID Valins atau '-' untuk mengakhiri: "
 			case userInput of
 				"-" -> do
@@ -23,8 +24,7 @@ mainProgram valinsCurrent mode = do
 					
 					-- let portNum = length currentList
 					let valinsPort = ValinsPort currentNum maybePortSN
-					let newValins = Valins (Just userInput) [valinsPort] (checkSignificant maybePortSN)
-					let newValinsCont = ValinsCont [valinsPort] [] Nothing [newValins]
+					let newValins = Valins (Just userInput) [valinsPort]
 					mainProgram newValins "current_valins"
 		"current_valins" -> do
 			let currentNum = getPortLength valinsCurrent
@@ -37,88 +37,77 @@ mainProgram valinsCurrent mode = do
 						let currentValID = getValinsID valinsCurrent
 						let maybePortSN = getPortSN portSN
 						let valinsPort = ValinsPort currentNum maybePortSN
-						let newValins = Valins (currentValID) (appendValins valinsPort currentPortList) (checkSignificant maybePortSN)
+						let newValins = Valins (currentValID) (appendValins valinsPort currentPortList)
 						mainProgram newValins "current_valins"
 				else 
 					do 
-						putStrLn "FINISH"
 						-- Write Valins to File
 						-- Print ke layar hasil yang sudah diinputkan
-						confirm <- getUserInput "Pengisian valins selesai, ingin menambahkan valins baru lagi?"
+						confirm <- getUserInput "Pengisian valins selesai, ingin menambahkan valins baru lagi? (Y/N)"
 						case confirm of
 							"Y" -> do
-								let newValins = createValins Nothing []
+								putStrLn "FINISH"
+								putStrLn (valinsToString valinsCurrent)
+								appendFile "valins_list.txt" (valinsToString valinsCurrent)
+								putStrLn "Write To File Done"
+								let newValins = Valins Nothing []
 								mainProgram (newValins) "new_valins"
 							"N" -> do
+								putStrLn "FINISH"
+								putStrLn (valinsToString valinsCurrent)
+								appendFile "valins_list.txt" (valinsToString valinsCurrent)
+								putStrLn "Write To File Done"
 								putStrLn "Bersiap untuk memproses valins"
+								let newValins = Valins Nothing []
+								mainProgram (newValins) "process_valins"
+							_ -> do
+								putStrLn "Input Salah, ingin menambahkan valins baru lagi? (Y/N)"
+								mainProgram valinsCurrent "current_valins"
 		"process_valins" -> do
 			putStrLn "Memproses summary valins"
+			fileContent <- openFile "valins_list.txt" ReadMode
+			let valinsCont = ValinsCont [] [] Nothing []
+			putStrLn "Reading File ..."
+			processValins valinsCont valinsCurrent fileContent
+
+
+-- Process Data Valins. Read dari file, create valins container, cek significant valins, cari unmapped SN, populate valins List
 
 
 
-getUserInput:: String -> IO String
-getUserInput str = do
-	putStr str
-	hFlush stdout
-	getLine
+processValins :: ValinsCont -> Valins -> Handle -> IO ()
+processValins vCont v inH = do 
+    ineof <- hIsEOF inH
+    if ineof
+    	then 
+    		putStrLn "N OF FILE"
+    	else
+    		do 
+    			inputStr <- hGetLine inH
+	    		case inputStr of
+	    			"---" -> do
+	    				putStrLn $ inputStr ++ " (IGNORE)"
+	    			(['v']['a']['l']['i']['n']['s']['_']['i']['d']['=']:id) -> do
+	    				putStrLn $ inputStr ++ " (FOUND VALINS ID)"
+	    			_ -> do 
+	    				putStrLn "End Of File"
+	    		processValins vCont v inH
+   --     if ineof
+   --         then {-return ()-} putStrLn "END"
+   --         else do 
+   --         	inpStr <- hGetLine inH
+			-- putStrLn inpStr
+			-- 	   -- processValins vCont v inH
 
--- ADT
-data ValinsPort = ValinsPort { portNum :: Int  
-                     , portSN :: Maybe String  
-                     } deriving (Show)
 
-data Valins = Valins {
-				valinsID :: Maybe String,
-				portList :: [ValinsPort],
-				contributeCount :: Int
-			} deriving (Show)
-
-data ValinsCont = ValinsCont {
-				portListCont :: [ValinsPort],
-				unmappedSN :: [String],
-				significantValins :: Maybe String,
-				valinsList :: [Valins]
-			} deriving (Show)
-
--- Function Constructor
-createValins::Maybe String -> [ValinsPort] -> Valins
-createValins (Just x) _ = Valins (Just x) [] 0
-createValins Nothing _ = Valins Nothing [] 0
-
-createValinsCont::ValinsCont
-createValinsCont = ValinsCont [] [] Nothing []
-
-createValinsPort::Int -> Maybe String -> ValinsPort
-createValinsPort port_num (Just sn) = ValinsPort port_num (Just sn)
-createValinsPort port_num (Nothing) = ValinsPort port_num (Nothing)
-
--- Function Processing
-getPortSN :: String -> Maybe String
-getPortSN x
-	| x /= "-" = Just x
-	| otherwise = Nothing
-
-getPortLength :: Valins -> Int
-getPortLength (Valins _ portL _) = (length portL) + 1
-
-getPortList :: Valins -> [ValinsPort]
-getPortList (Valins _ portL _) = portL
-
-getValinsID :: Valins -> Maybe String
-getValinsID (Valins valID _ _) = valID
-
-checkSignificant :: Maybe String -> Int
-checkSignificant (Just x) = 1
-checkSignificant Nothing = 0
-
-appendValins:: ValinsPort -> [ValinsPort] -> [ValinsPort]
-appendValins new [] = [new]
-appendValins new (x:xs) = x : appendValins new xs
 
 -- Main
 main :: IO ()
 main = do
-	let valins_current = createValins Nothing []
+	putStrLn "----------------------------------------"
+	putStrLn "----------- VALINS PROCESSOR -----------"
+	putStrLn "----------------------------------------"
+	let valins_current = Valins Nothing []
 	mainProgram valins_current "new_valins"
 
 
